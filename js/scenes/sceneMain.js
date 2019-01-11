32ccc74d63ebf47372ee8c1ddfcb2b1ee817411f
Pål Grønstad Solheim <paul.solheim@gmail.com>
2019-01-11 02:06:49 +0100

    Update sceneMain.js
    flyttet SoundButtons.

--- js/scenes/sceneMain.js
+++ js/scenes/sceneMain.js
@@ -4,21 +4,20 @@ class SceneMain extends Phaser.Scene {
     }
     preload() {
     }
-    
+
     create() {
          //set up
         emitter=new Phaser.Events.EventEmitter();
         controller=new Controller();
         mediaManager=new MediaManager({scene:this});
+        var sb=new SoundButtons({scene:this});
         //
         //define our objects
         this.road = new Road({
             scene: this
         });
         this.road.x = game.config.width * .5;
-
-        var sb=new SoundButtons({scene:this});
-
+        
     }
 
     update() {
