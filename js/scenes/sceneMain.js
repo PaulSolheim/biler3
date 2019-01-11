6b6cda60b135a5f9caae8dcb3643129752f84f11
Pål Grønstad Solheim <paul.solheim@gmail.com>
2019-01-11 01:21:01 +0100

    Lagt til fra Toolbox
    Lagt til alle filene fra toolbox prosjektet for å få tid til å komme i mål.

---
+++ js/scenes/sceneMain.js
@@ -0,0 +1,27 @@
+class SceneMain extends Phaser.Scene {
+    constructor() {
+        super('SceneMain');
+    }
+    preload() {
+    }
+    
+    create() {
+         //set up
+        emitter=new Phaser.Events.EventEmitter();
+        controller=new Controller();
+        mediaManager=new MediaManager({scene:this});
+        //
+        //define our objects
+        this.road = new Road({
+            scene: this
+        });
+        this.road.x = game.config.width * .5;
+
+        var sb=new SoundButtons({scene:this});
+
+    }
+
+    update() {
+        //constant running loop
+    }
+}
